359b117ed7fd24912363a7eda0b0cd7c15c41282
fikriks <fikriks130@gmail.com>
2025-04-24 15:44:49 +0700

    fix: uncomment edit button in company info view
    The edit button was previously commented out, preventing users from updating company information. This commit restores the button to enable editing functionality.

--- app/Views/info/index.php
+++ app/Views/info/index.php
@@ -53,11 +53,11 @@
     <h4>Deskripsi Perusahaan</h4>
         <p><?= $row['deskripsi'] ?></p>
     </div> <br>
-    <!-- <div class="edit">
+    <div class="edit">
     <button type="button" class="btn btn-sm btn-warning" data-toggle="modal" data-target="#modalUpdate" id="btn-edit-info" 
         data-id="<?= $row['id']?>" data-nama="<?= $row['nama']?>" data-foto="<?= $row['foto']?>" data-deskripsi="<?= $row['deskripsi']?>">
         <i class="fa fa-edit"></i></button>
-    </div> -->
+    </div>
     <?php } ?>
 </div>
 <!-- End of Main Content -->
